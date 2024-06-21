41fd42c997b3fb04bc39d59f7921dfca85c437c1
AnshARao <115018391+AnshARao@users.noreply.github.com>
2024-06-20 23:30:08 -0400

    removed spaces, I'm a dumbass
    kms

---
+++ Lem-Cloudy-Lib/src/devices.cpp
@@ -0,0 +1,73 @@
+#include "main.h"
+#include "LemLib/api.hpp"
+
+//creates controller
+
+//creates motor variables with specified ports and gearsets
+pros::Motor LF_motor(PORT_LF, pros::MotorGears::blue);
+pros::Motor LM_motor(PORT_LM, pros::MotorGears::blue);;
+pros::Motor LB_motor(PORT_LB, pros::MotorGears::blue);;
+pros::Motor RF_motor(PORT_RF, pros::MotorGears::blue);;
+pros::Motor RM_motor(PORT_RM, pros::MotorGears::blue);;
+pros::Motor RB_motor(PORT_RB, pros::MotorGears::blue);;
+
+//creates left motor groups with inducidual motors listed above
+pros::MotorGroup left_chassis({ PORT_LF, PORT_LM, PORT_LB }, pros::MotorGearset::blue);
+//creates right motor groups with inducidual motors listed above
+pros::MotorGroup right_chassis({ PORT_RF, PORT_RM, PORT_RB }, pros::MotorGearset::blue);
+
+//creates drivetrain with certain variables listed below
+lemlib::Drivetrain drivetrain(&left_chassis, // left motor group
+                              &right_chassis, // right motor group
+                              13, // 10 inch track width
+                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
+                              450, // drivetrain rpm is 450
+                              2 // horizontal drift is 2 (for now)
+);
+
+//creates imu sensor with specified port
+pros::Imu IMU(PORT_IMU);
+
+//creates rotation sensors for odom with specified ports
+pros::Rotation odom_vert_sensor(PORT_ODOM_VERT);
+pros::Rotation odom_hozi_sensor(PORT_ODOM_HORI);
+
+//creates odom tracking wheel configuration with specified rotation sensors, wheel sizes, and offsets
+lemlib::TrackingWheel odom_vert_wheel(&odom_vert_sensor, lemlib::Omniwheel::NEW_2, VERTICAL_OFFSET);
+lemlib::TrackingWheel odom_hori_wheel(&odom_hozi_sensor, lemlib::Omniwheel::NEW_2, HORIZONTAL_OFFSET);
+
+
+//creates entire odom sensor variable with all classes above
+lemlib::OdomSensors odom_sensors(&odom_vert_wheel, nullptr, &odom_hori_wheel, nullptr, &IMU);
+
+//creates lateral PID controller variable
+lemlib::ControllerSettings lateral_controller(
+    10, // proportional gain (kP)
+    0, // integral gain (kI)
+    3, // derivative gain (kD)
+    3, // anti windup
+    1, // small error range, in inches
+    100, // small error range timeout, in milliseconds
+    3, // large error range, in inches
+    500, // large error range timeout, in milliseconds
+    20 // maximum acceleration (slew)
+);
+
+//creates angular PID controller variable
+lemlib::ControllerSettings angular_controller(
+    2, // proportional gain (kP)
+    0, // integral gain (kI)
+    10, // derivative gain (kD)
+    3, // anti windup
+    1, // small error range, in degrees
+    100, // small error range timeout, in milliseconds
+    3, // large error range, in degrees
+    500, // large error range timeout, in milliseconds
+    0 // maximum acceleration (slew)
+);
+
+//creates an exponential drive curve function variable with specified deadzone, minimum output, and curve.
+lemlib::ExpoDriveCurve lateral_curve(3, 10, DRIVE_CURVE);
+
+//creates chassis variable with all listed above
+lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, odom_sensors, &lateral_curve);
