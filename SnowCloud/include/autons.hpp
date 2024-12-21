1db9c921aa1753e4f9dfe63d087b86480c65a27a
DevinW-2072 <dedevdevin@gmail.com>
2024-12-20 21:32:14 -0500

    12/20

---
+++ SnowCloud/include/autons.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "EZ-Template/drive/drive.hpp"
+
+extern Drive chassis;
+
+void drive_example();
+void turn_example();
+void drive_and_turn();
+void wait_until_change_speed();
+void swing_example();
+void motion_chaining();
+void combining_movements();
+void interfered_example();
+
+void redNeg2();
+void redNeg3();
+void redNeg4();
+void redNeg5();
+void SoloAwpRedPos();
+void blueNeg3();
+void blueNeg4();
+void blueNeg5();
+void goalRush();
+void skills();
+void elims();
+void SoloAwpRedNeg();
